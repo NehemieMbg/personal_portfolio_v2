0a01ba2b2f7b3ecd66cffe573b15cfa1dc7448d1
NehemieMbg <nehemie.mbg@gmail.com>
2023-11-07 11:07:20 +0100

    about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,120 @@
+import Image from 'next/image';
+import { about } from '../constants';
+
 const page = () => {
-  return <section className="max-w-wide mx-auto w-full pb-20">page</section>;
+  return (
+    <section className="max-w-wide mx-auto w-full pb-20">
+      <h2 className="">{about.title}</h2>
+
+      <div className="layout">
+        <div className="flex gap-10">
+          <div className="flex flex-col gap-6 font-light text-neutral-400 w-1/2 pr-10">
+            {about.text.map((text, index) => (
+              <p key={index}>{text}</p>
+            ))}
+          </div>
+          <div className="w-1/2">
+            <div className="w-full max-h-[311px] overflow-hidden rounded-xl bg-neutral-300 mb-8">
+              <Image
+                src={about.image}
+                alt={about.title}
+                layout="responsive"
+                width={492}
+                height={311}
+                className="object-center object-cover w-full max-h-[311px]"
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-8">
+              <div className="font-light">
+                <h4 className="text-sm text-neutral-400 font-light mb-1">
+                  Email
+                </h4>
+                <a
+                  href={'mailto:nehemie.mbg@gmail.com'}
+                  target="_blank"
+                  className="w-max hover:text-red-400 transition-colors duration-200"
+                >
+                  {about.email}
+                </a>
+              </div>
+
+              <div className="font-light">
+                <h4 className="text-sm text-neutral-400 font-light mb-1">
+                  Phone
+                </h4>
+                <a
+                  href={`tel:${about.phone}`}
+                  target="_blank"
+                  className="w-max hover:text-red-400 transition-colors duration-200"
+                >
+                  {about.phone}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-14">
+          <h3>Skills</h3>
+
+          <div className="grid grid-cols-2 gap-8">
+            <div className="font-light flex flex-col gap-8">
+              {about.skills[0].map((skill, index) => (
+                <p key={skill} className="pb-8 border-b border-b-neutral-800">
+                  {skill}
+                </p>
+              ))}
+            </div>
+
+            <div className="font-light flex flex-col gap-8">
+              {about.skills[1].map((skill, index) => (
+                <p key={skill} className="pb-8 border-b border-b-neutral-800">
+                  {skill}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-14">
+          <div className="flex justify-between">
+            <h3>Experience</h3>
+            <a
+              href={about.resume}
+              target="_blank"
+              download={about.resume}
+              className="bg-dark-gray py-3.5 px-6 border border-neutral-800 cursor-pointer rounded-full hover:bg-neutral-900 transition-colors duration-200"
+            >
+              Download CV
+            </a>
+          </div>
+
+          <div className="flex flex-col gap-8">
+            {about.experience.map((experience, index) => (
+              <div key={index} className="flex gap-10 font-light">
+                <p className="min-w-[240px] text-neutral-400">
+                  {experience.date}
+                </p>
+
+                <div className="flex flex-col gap-6 w-full">
+                  <div>
+                    <h4 className="mb-1">{experience.company}</h4>
+                    <p className="text-sm text-neutral-400">
+                      {experience.position}
+                    </p>
+                  </div>
+
+                  <p className="font-light text-neutral-400 leading-7">
+                    {experience.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default page;
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -137,3 +137,65 @@ export const work = {
     },
   ],
 };
+
+export const about = {
+  title: 'About me',
+  text: [
+    "I'm Nehemie Mombanga, hailing from the creative heart of Lille, France, where I harmonize my graphic design heritage with robust software engineering skills to construct innovative applications. My professional path reflects a commitment to blending visual storytelling with technical prowess, addressing complex challenges with solutions that are as functional as they are aesthetically compelling.",
+    'Curiosity fuels my continuous pursuit of knowledge, leading me through a career that spans from crafting visual identities to programming full-stack applications. I thrive in collaborative settings, championing the synergy of ideas and the strength found in diversity to forge applications that resonate with users and stand the test of time.',
+    'My dedication to clear communication and empathetic engagement has been instrumental in forging lasting connections, guiding my transition from the precision of design to the logic of coding. This journey has equipped me with the versatility to serve an array of clients, ensuring that each project not only meets but exceeds the vision and requirements laid out before me.',
+  ],
+  email: 'nehemie.mbg@gmail.com',
+  phone: '+33 7 69 35 74 53',
+  resume: '/resume/nehemie_mombanga.pdf',
+  image: '/images/nehemie-mombanga.jpg',
+  skills: [
+    [
+      'JavaScript / TypeScript',
+      'Solidity',
+      'Python',
+      'C',
+      'HTML',
+      'CSS',
+      'SQL',
+    ],
+    ['React', 'Next', 'Node', 'Express', 'Git', 'Figma', 'Linux'],
+  ],
+  experience: [
+    {
+      date: 'May 2022 - Present',
+      company: 'Holberton School',
+      position: 'Student Software Engineer',
+      description:
+        "Mastered a comprehensive skill set in full-stack development and software engineering, encompassing back-end logic and front-end aesthetics, with a strong emphasis on data privacy and algorithmic efficiency. Fostered a collaborative educational environment through mentoring, and propelled the school's expansion and enrollment by promoting its values and programs in various cities.",
+    },
+    {
+      date: 'December 2018 - August 2021',
+      company: 'Green Yard Frozen',
+      position: 'Production Operator',
+      description:
+        'Oversaw recipe preparation and ensured hygienic machine operations to uphold production standards and prevent contamination, enhancing client satisfaction. Provided comprehensive training and guidance to new workers, instilling best practices and proficient task management.',
+    },
+    {
+      date: 'June 2018 - July 2018',
+      company: 'Pano',
+      position: 'Intern Graphic Designer',
+      description:
+        'Contributed to business growth through the creation of impactful signaletics and visually engaging designs.',
+    },
+    {
+      date: 'June 2017 - July 2017',
+      company: 'Made In V',
+      position: 'Intern Graphic Designer',
+      description:
+        'Designed a standout logo for Grand Nord Automobile, boosting market visibility for the renowned car reseller in northern France. Crafted an inclusive and accessible logo for a wheelchair-friendly golf event, highlighting the importance of diversity in design.',
+    },
+    {
+      date: 'June 2016 - July 2016',
+      company: '2D Sign',
+      position: 'Intern Graphic Designer',
+      description:
+        'Skillfully crafted 2D signaletics to amplify business visibility and branding for clients, aiding them in establishing a distinct presence in their respective markets.',
+    },
+  ],
+};

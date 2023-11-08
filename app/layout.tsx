2486f49cc9c775015ce345003f7a094d20c150b0
NehemieMbg <nehemie.mbg@gmail.com>
2023-11-08 20:18:35 +0100

    Analetics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer, Navbar } from './components';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
